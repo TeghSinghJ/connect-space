1871db9a58771ffb05794650a53fab84d358fa82
Juni Tegh Singh <81065036+TeghSinghJ@users.noreply.github.com>
2024-06-29 00:00:54 +0530

    Fixed icons in Navbar Component and Homepage cards

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -26,7 +26,7 @@ const MeetingModal = ({ isOpen, title, image, children, isClose, buttonIcon, han
     return (
         <div>
             <Dialog open={isOpen} onOpenChange={isClose}>
-                <DialogTrigger>Open</DialogTrigger>
+                {/* <DialogTrigger>Open</DialogTrigger> */}
                 <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
                     <div className='flex flex-col gap-6'>
                         {
